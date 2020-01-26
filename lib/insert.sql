406bb81518b94fba5b2927c8d2d000a964ee008f
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-26 00:08:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears(id, name, age, gender, color, temperament, alive)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Angry", TRUE);
